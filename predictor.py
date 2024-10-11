fb4ac141c5fdaa3206adedb2bcfb4a10639a5c77
zms <zms>
2024-10-11 14:50:42 +0800

    x

--- predictor.py
+++ predictor.py
@@ -1,9 +1,16 @@
 import argparse
+import os
 
+import numpy as np
 import torch
+from pydub import AudioSegment
+from scipy.io import wavfile
+from torch import LongTensor, no_grad
 
+import commons
 import utils
 from models import SynthesizerTrn
+from text import text_to_sequence
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -22,6 +29,31 @@ class Predictor:
         _ = self.net_g.eval()
         _ = utils.load_checkpoint(model_path, self.net_g, None)
 
+    def get_text(self, text, is_symbol):
+        text_norm = text_to_sequence(text, self.hps.symbols, [] if is_symbol else self.hps.data.text_cleaners)
+        if self.hps.data.add_blank:
+            text_norm = commons.intersperse(text_norm, 0)
+        text_norm = LongTensor(text_norm)
+        return text_norm
+
+    def tts_fn_id(self, text, n_speaker_id, output_path):
+        if text is not None:
+            text = "[EN]" + text + "[EN]"
+        stn_tst = self.get_text(text, False)
+        with no_grad():
+            x_tst = stn_tst.unsqueeze(0).to(device)
+            x_tst_lengths = LongTensor([stn_tst.size(0)]).to(device)
+            sid = LongTensor([n_speaker_id]).to(device)
+            audio = self.net_g.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667, noise_scale_w=0.8,
+                                     length_scale=1.0 / self.speed)[0][0, 0].data.cpu().float().numpy()
+        del stn_tst, x_tst, x_tst_lengths, sid
+        audio = np.int16(audio * 32768)
+        wavfile.write(output_path, 22050, audio)
+        audio = AudioSegment.from_wav(output_path)
+        os.remove(output_path)
+        output_path = output_path.replace('.wav', '.mp3')
+        audio.export(output_path, format="mp3")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -31,3 +63,4 @@ if __name__ == '__main__':
     print("model_path:", args.model_path)
     print("config_path:", args.config_path)
     predictor = Predictor(args.model_path, args.config_path)
+    predictor.tts_fn_id("hello world", 0, "./output.wav")
